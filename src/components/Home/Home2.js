9c85d0ce25fc33212ba4a0d41e38e5986a87748b
JERONIMO MAYORCA <mayorcajeronimo@gmail.com>
2024-06-11 12:37:49 -0500

    update home view

---
+++ src/components/Home/Home2.js
@@ -0,0 +1,86 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import myImg from "../../Assets/avatar.svg";
+import Tilt from "react-parallax-tilt";
+import {
+  AiFillGithub,
+  AiOutlineTwitter,
+  AiFillInstagram,
+} from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+
+function Home2() {
+  return (
+    <Container fluid className="home-about-section" id="about">
+      <Container>
+        <Row>
+          <Col md={8} className="home-about-description">
+            <h1 style={{ fontSize: "2.6em" }}>
+              LET ME <span className="blue"> INTRODUCE </span> MYSELF
+            </h1>
+            <p className="home-about-body">
+              During my career as a developer, I've worked with a wide range of technologies, tools, and programming languages like
+              <i>
+                <b className="blue"> Python, Javascript, Angular, Django, Next.js, Tailwind CSS, Frappe. </b>
+              </i>
+              and more
+              <br />
+              <br />
+              What drives my soul is building products that provide value to many people.&nbsp;
+              <br />
+              <i>
+                I want to make an <b className="blue">impact on the world</b>{" "} with my work.
+              </i>
+            </p>
+          </Col>
+          <Col md={4} className="myAvtar">
+            <Tilt>
+              <img src={myImg} className="img-fluid" alt="avatar" />
+            </Tilt>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={12} className="home-about-social">
+            <h1>FIND ME ON</h1>
+            <p>
+              Feel free to <span className="blue">connect </span>with me
+            </p>
+            <ul className="home-about-social-links">
+              <li className="social-icons">
+                <a
+                  href="https://github.com/jeronimomayorca"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <AiFillGithub />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://www.linkedin.com/in/mayorcajeronimo/"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <FaLinkedinIn />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://www.instagram.com/jeronimomayorcaar/"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour home-social-icons"
+                >
+                  <AiFillInstagram />
+                </a>
+              </li>
+            </ul>
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+export default Home2;
