98cd2a2c1d5b9fc823eb33837935f88a961d8793
jeronimomayorca <mayorcajeronimo@gmail.com>
2024-07-30 21:06:17 -0500

    update cv and refactor layout of my projects

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -3,38 +3,53 @@ import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import { CgWebsite } from "react-icons/cg";
 import { BsGithub } from "react-icons/bs";
+import { Row, Col } from "react-bootstrap";
 
 function ProjectCards(props) {
   return (
     <Card className="project-card-view">
-      <Card.Img variant="top" src={props.imgPath} alt="card-img" />
-      <Card.Body>
-        <Card.Title>{props.title}</Card.Title>
-        <Card.Text style={{ textAlign: "justify" }}>
-          {props.description}
-        </Card.Text>
-        <Button variant="primary" href={props.ghLink} target="_blank">
-          <BsGithub /> &nbsp;
-          {props.isBlog ? "Blog" : "GitHub"}
-        </Button>
-        <Card.Footer className="text-2xl">{ props.tools }</Card.Footer>
-        {"\n"}
-        {"\n"}
+      <Row>
+        <Col md={5}>
+          <Card.Img variant="top" src={props.imgPath} alt="card-img" style={{ width: "100 %" }} />
+        </Col>
+        <Col md={7}>
+          <Card.Body>
+            <Card.Title>{props.title}</Card.Title>
+            <Card.Text style={{ textAlign: "right" }}>
+              {props.description}
+              <br />
+              <br />
+              Technical stack:
+              <br />
+              <ul style={{ listStyleType: "none" }}>
+                {props.stack.map((tech, index) => (
+                  <li key={index}>{tech} -</li>
+                ))}
+              </ul>
+            </Card.Text>
+            <Button variant="primary" href={props.ghLink} target="_blank">
+              <BsGithub /> &nbsp;
+              {props.isBlog ? "Blog" : "GitHub"}
+            </Button>
+            {"\n"}
+            {"\n"}
 
-        {/* If the component contains Demo link and if it's not a Blog then, it will render the below component  */}
+            {/* If the component contains Demo link and if it's not a Blog then, it will render the below component  */}
 
-        {!props.isBlog && props.demoLink && (
-          <Button
-            variant="primary"
-            href={props.demoLink}
-            target="_blank"
-            style={{ marginLeft: "10px" }}
-          >
-            <CgWebsite /> &nbsp;
-            {"Demo"}
-          </Button>
-        )}
-      </Card.Body>
+            {!props.isBlog && props.demoLink && (
+              <Button
+                variant="primary"
+                href={props.demoLink}
+                target="_blank"
+                style={{ marginLeft: "10px" }}
+              >
+                <CgWebsite /> &nbsp;
+                {"Demo"}
+              </Button>
+            )}
+          </Card.Body>
+        </Col>
+      </Row>
     </Card>
   );
 }
--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -3,7 +3,6 @@ import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
 import cv_ingles from "../../Assets/cv_ingles.pdf";
-import cv_espanol from "../../Assets/cv_espanol.pdf"
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
@@ -25,6 +24,9 @@ function ResumeNew() {
           <Document file={cv_ingles} className="d-flex justify-content-center">
             <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
           </Document>
+          <Document file={cv_ingles} className="d-flex justify-content-center">
+            <Page pageNumber={2} scale={width > 786 ? 1.7 : 0.6} />
+          </Document>
         </Row>
 
         <Row style={{ justifyContent: "center", position: "relative", gap: "40px" }}>
@@ -35,18 +37,9 @@ function ResumeNew() {
             style={{ maxWidth: "250px" }}
           >
             <AiOutlineDownload />
-            &nbsp;Download CV (english)
+            &nbsp;Download CV
           </Button>
 
-          <Button
-            variant="primary"
-            href={cv_espanol}
-            target="_blank"
-            style={{ maxWidth: "250px" }}
-          >
-            <AiOutlineDownload />
-            &nbsp;Descargar CV (español)
-          </Button>
         </Row>
       </Container>
     </div>
